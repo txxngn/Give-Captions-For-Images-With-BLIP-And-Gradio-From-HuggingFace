e68c33316c8fc774607e5dbceb1c0de3affdcfd3
Thai Nguyen <thainguyen20594@gmail.com>
2024-12-19 20:24:40 -0500

    Initial Steps

---
+++ image_cap.py
@@ -0,0 +1,40 @@
+import requests
+from PIL import Image #PIL = Python Image Library
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+#1.
+#Load the pretrained processor and model
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+#2.
+# Load your image, DONT FORGET TO WRITE YOUR IMAGE NAME
+img_path = "snow-leopard.png"
+# convert it into an RGB format 
+image = Image.open(img_path).convert('RGB')
+
+
+#3.
+# You do not need a question for image captioning
+text = "the image of"
+inputs = processor(images=image, text=text, return_tensors="pt")
+
+
+#4.
+# Generate a caption for the image
+outputs = model.generate(**inputs, max_length=50)
+
+
+#5.
+# Decode the generated tokens to text
+caption = processor.decode(outputs[0], skip_special_tokens=True)
+# Print the caption
+print(caption)
+
+
+
+
+
+
+
+
